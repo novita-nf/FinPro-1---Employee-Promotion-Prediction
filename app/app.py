d83dc0f0d1cc682f97066d021fa7f63e69ca7643
novita-nf <novitanurulf@gmail.com>
2025-11-28 12:03:04 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -38,7 +38,7 @@ else:
     BASE_DIR = Path(os.getcwd())
 
 data_path = BASE_DIR.parent / "Data" / "Rakamin Bootcamp - Dataset - Promotion Dataset.csv"
-model_path = BASE_DIR / "model2.pkl"
+model_path = BASE_DIR / "model3.pkl"
 
 df = pd.read_csv(data_path, sep=";")
 with open(model_path, "rb") as f:
